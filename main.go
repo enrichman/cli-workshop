26a214cb102b011bbd15074cbf75d2287011dfc7
Enrico Candino <enrico.candino@gmail.com>
2023-09-10 00:13:51 +0200

    section-03

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	resp, err := http.Get("https://api.github.com/users/enrichman")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user map[string]any
+	err = json.Unmarshal(bodyBytes, &user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("User '%v' (%v) found\n", user["login"], user["name"])
 }
